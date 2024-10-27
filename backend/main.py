ef610b0f4e7a8b73d3b42e981c23bb3f8ca11b44
Lucas Abdo <lucastroa94+github@gmail.com>
2024-10-27 19:53:32 -0300

    Add greet route

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello, FastAPI!"}
+
+@app.get("/greet/")
+async def greet_user(username: str):
+    return {"message": f"Hello, {username}"}

c37147ce20f7c637a6d4c6e8c83bcbbd7e921e0a
Lucas Abdo <lucastroa94+github@gmail.com>
2024-10-29 00:05:18 -0300

    Add test to button element

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders button without click', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const buttonElement = screen.getByText(/click me/i);
+  const countText = screen.getByText(/button clicked 0 times/i);
+  
+  expect(buttonElement).toBeInTheDocument();
+  expect(countText).toBeInTheDocument();
 });
+
+test('renders button with one click', () => {
+  render(<App />)
+
+  const buttonElement = screen.getByText(/click me/i);
+  
+  fireEvent.click(buttonElement);
+
+  expect(buttonElement).toBeInTheDocument();
+  expect(screen.getByText(/button clicked 1 times/i)).toBeInTheDocument();
+})

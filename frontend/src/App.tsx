d76c6fe1ab86f9d0a5d2f36d8d37cbed1ce57e5c
Lucas Abdo <lucastroa94+github@gmail.com>
2024-10-29 00:05:04 -0300

    Add simple button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  
+  const handleClick = () => {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Button clicked {count} times</p>
+        <button onClick={handleClick}>Click me</button>
       </header>
     </div>
   );

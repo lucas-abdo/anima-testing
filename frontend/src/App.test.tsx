addac6b37dc1d95f516fc817bbb88987fdd7bf3b
Lucas Abdo <lucastroa94+github@gmail.com>
2024-10-30 20:50:28 -0300

    Fix tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -6,13 +6,13 @@ test('renders button without click', () => {
   render(<App />);
 
   const buttonElement = screen.getByText(/click me/i);
-  const countText = screen.getByText(/button clicked 0 times/i);
+  const countText = screen.getByText(/button result: 0/i);
   
   expect(buttonElement).toBeInTheDocument();
   expect(countText).toBeInTheDocument();
 });
 
-test('renders button with one click', () => {
+test('renders button with one click to increment counter', () => {
   render(<App />)
 
   const buttonElement = screen.getByText(/click me/i);
@@ -20,5 +20,19 @@ test('renders button with one click', () => {
   fireEvent.click(buttonElement);
 
   expect(buttonElement).toBeInTheDocument();
-  expect(screen.getByText(/button clicked 1 times/i)).toBeInTheDocument();
+  expect(screen.getByText(/button result: 1/i)).toBeInTheDocument();
+})
+
+test('renders button with two clicks to increment and decrement counter', () => {
+  render(<App />)
+
+  const incrementButtonElement = screen.getByText(/click me/i);
+  const subtractButtonElement = screen.getByText(/subtract/i);
+  
+  fireEvent.click(incrementButtonElement);
+  fireEvent.click(subtractButtonElement);
+
+  expect(incrementButtonElement).toBeInTheDocument();
+  expect(subtractButtonElement).toBeInTheDocument();
+  expect(screen.getByText(/button result: 0/i)).toBeInTheDocument();
 })
